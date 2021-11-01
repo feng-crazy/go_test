cfd9b103d141b3be5652914ff3731258883e7dbf
何登锋 <t-hedengfeng@megvii.com>
2021-11-01 17:03:14 +0800

    ants test

--- worker_pool_test/ants_test.go
+++ worker_pool_test/ants_test.go
@@ -23,7 +23,7 @@ func demoFunc() {
 	fmt.Println("Hello World!")
 }
 
-func TestAnts(t *testing.T) {
+func TestAntsCommonPool(t *testing.T) {
 	defer ants.Release()
 
 	runTimes := 1000
@@ -41,7 +41,11 @@ func TestAnts(t *testing.T) {
 	wg.Wait()
 	fmt.Printf("running goroutines: %d\n", ants.Running())
 	fmt.Printf("finish all tasks.\n")
+}
 
+func TestAntsPoolWithMethod(t *testing.T) {
+	runTimes := 1000
+	var wg sync.WaitGroup
 	// Use the pool with a method,
 	// set 10 to the capacity of goroutine pool and 1 second for expired duration.
 	p, _ := ants.NewPoolWithFunc(10, func(i interface{}) {
@@ -60,4 +64,26 @@ func TestAnts(t *testing.T) {
 	if sum != 499500 {
 		panic("the final result is wrong!!!")
 	}
+}
+
+func TestAntsPool(t *testing.T) {
+	runTimes := 1000
+	var wg sync.WaitGroup
+	// Use the pool
+	// set 10 to the capacity of goroutine pool and 1 second for expired duration.
+	p1, _ := ants.NewPool(10)
+	defer p1.Release()
+	// Submit tasks one by one.
+
+	syncCalculateSum := func() {
+		demoFunc()
+		wg.Done()
+	}
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		_ = p1.Submit(syncCalculateSum)
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", p1.Running())
+	fmt.Printf("finish all tasks.\n")
 }
